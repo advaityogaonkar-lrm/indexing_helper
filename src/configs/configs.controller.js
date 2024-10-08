25180af0d80b61fc5eb3eb4bcb450197a9794cbf
advait.yogaonkar <advait.yogaonkar@leftrightmind.com>
2024-10-08 12:23:40 +0530

    data result

--- src/configs/configs.controller.js
+++ src/configs/configs.controller.js
@@ -42,7 +42,7 @@ export async function createReindexingJobList(req, res, next) {
     res.status(200).json({
       status: "success",
       data: {
-        data: result,
+        result,
         message: "Job list created",
       },
     });
